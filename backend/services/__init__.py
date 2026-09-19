e9e97aaf9e413c0d75a832e9a90158d0fe12a0df
pradeesht22-bit <pradeesht22@gmail.com>
2026-09-19 16:36:33 +0530

    Create __init__.py

---
+++ backend/services/__init__.py
@@ -0,0 +1,8 @@
+from .analysis_service import (
+    get_asset_analysis_service,
+    get_risk_analysis_service,
+    get_indicator_analysis_service,
+    get_correlation_analysis_service,
+    get_asset_comparison_service,
+    analyze_all_assets,
+)
