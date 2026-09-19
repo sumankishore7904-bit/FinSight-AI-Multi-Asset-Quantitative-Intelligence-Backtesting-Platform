bc6a533da3bca24a010b67ebcc59bf68c41923b5
aatif902008-taj <aatif.90.2008@gmail.com>
2026-09-19 17:07:43 +0530

    Create __init__.py

---
+++ backend/data/__init__.py
@@ -0,0 +1,9 @@
+from .loader import load_asset_data
+from .cleaner import clean_market_data
+from .data_service import get_asset_data
+
+__all__ = [
+    "load_asset_data",
+    "clean_market_data",
+    "get_asset_data",
+]
